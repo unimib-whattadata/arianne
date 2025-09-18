75e994d23650263eb399fbfec7926f5434056450
Giulia Volonté <giulia.volonte@unimib.it>
2025-09-18 18:00:53 +0200

    feat: add Logo component and integrate into Navbar and CardOption

---
+++ apps/psitools-patients/src/features/onboarding/navbar.tsx
@@ -0,0 +1,33 @@
+import { Logo } from '@/components/logo';
+import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
+
+export const Navbar = () => {
+  return (
+    <header className="fixed top-0 left-0 z-50 w-full p-4">
+      <div className="bg-background border-secondary container mx-auto rounded-full border px-4 py-2">
+        <nav className="hidden items-center justify-between lg:flex">
+          <div className="flex items-center gap-6">
+            {/* Logo */}
+            <Link href="/">
+              <Logo className="text-primary h-6" />
+            </Link>
+          </div>
+          <Button className="rounded-full">Esci</Button>
+        </nav>
+
+        {/* Mobile Menu */}
+        <div className="block lg:hidden">
+          <div className="flex items-center justify-between">
+            <Link href="/">
+              <Logo className="text-primary h-6" />
+            </Link>
+
+            <Button className="rounded-full">Esci</Button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
