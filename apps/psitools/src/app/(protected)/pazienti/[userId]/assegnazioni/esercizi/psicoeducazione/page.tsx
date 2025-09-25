6f0621af18cac2f340dd9307b8ab61a53a144e83
Andrea Feliziani <andrea.feliziani97@gmail.com>
2025-09-25 16:40:31 +0200

    feat(arianne): dynamic page

--- apps/psitools/src/app/(protected)/pazienti/[userId]/assegnazioni/esercizi/psicoeducazione/page.tsx
+++ apps/psitools/src/app/(protected)/pazienti/[userId]/assegnazioni/esercizi/psicoeducazione/page.tsx
@@ -1,12 +1,4 @@
 'use client';
-
-import {
-  Accordion,
-  AccordionItem,
-  AccordionTrigger,
-  AccordionContent,
-} from '@/components/ui/accordion';
-
 import { LessonCard } from '@/features/exercises/components/lesson-card';
 
 export default function psicoeducazione() {
@@ -16,33 +8,11 @@ export default function psicoeducazione() {
         <h1 className="text-xl font-semibold">Assegnazioni</h1>
       </div>
 
-      <Accordion type="multiple" className="w-full">
-        <AccordionItem value="ansia">
-          <AccordionTrigger className="flex items-center justify-between border-none bg-transparent hover:no-underline focus:no-underline">
-            <div>Ansia</div>
-          </AccordionTrigger>
-
-          <AccordionContent className="flex flex-col">
-            <LessonCard
-              id="meditazione"
-              title="Lezione 1: Combattere l’ansia con la meditazione"
-              lastView="23/02/2025"
-            />
-            <LessonCard
-              id="lezione2"
-              title="Lezione 2: Gestire l’ansia prima di un viaggio"
-              lastView="23/02/2025"
-            />
-          </AccordionContent>
-        </AccordionItem>
-
-        <AccordionItem value="alimentazione">
-          <AccordionTrigger className="flex items-center justify-between border-none bg-transparent hover:no-underline focus:no-underline">
-            <div>Alimentazione</div>
-          </AccordionTrigger>
-          <AccordionContent></AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      <LessonCard
+        id="meditazione"
+        title="Lezione 1: Combattere l’ansia con la meditazione"
+        lastView="23/02/2025"
+      />
     </div>
   );
 }
