8adce3b2ac1edbdfd821ba1cb589d56060dfab32
Fabio <fabio.dadda@unimib.it>
2025-10-06 18:04:01 +0200

    fix: fix conflicts

--- packages/db/supabase/migrations/0011_add_timestamp.sql
+++ packages/db/supabase/migrations/0011_add_timestamp.sql
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-ALTER TABLE "arianne_messages" ALTER COLUMN "created_at" SET DATA TYPE timestamp;
---> statement-breakpoint
-ALTER TABLE "arianne_messages" ALTER COLUMN "updated_at" SET DATA TYPE timestamp;
---> statement-breakpoint
-ALTER TABLE "arianne_diaries" ALTER COLUMN "content" SET DATA TYPE text;
-=======
 ALTER TABLE "arianne_messages" ALTER COLUMN "created_at" SET DATA TYPE timestamp;--> statement-breakpoint
 ALTER TABLE "arianne_messages" ALTER COLUMN "updated_at" SET DATA TYPE timestamp;--> statement-breakpoint
 ALTER TABLE "arianne_diaries" ALTER COLUMN "content" SET DATA TYPE text;
->>>>>>> dev
