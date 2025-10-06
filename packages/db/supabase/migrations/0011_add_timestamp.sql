ce8bcda9e5b878998ea28fb7c367d4f37367c90a
Fabio <fabio.dadda@unimib.it>
2025-10-06 18:00:51 +0200

    fix:timestamp

---
+++ packages/db/supabase/migrations/0011_add_timestamp.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "arianne_messages" ALTER COLUMN "created_at" SET DATA TYPE timestamp;
+--> statement-breakpoint
+ALTER TABLE "arianne_messages" ALTER COLUMN "updated_at" SET DATA TYPE timestamp;
+--> statement-breakpoint
+ALTER TABLE "arianne_diaries" ALTER COLUMN "content" SET DATA TYPE text;
